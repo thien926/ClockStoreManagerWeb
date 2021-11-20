d2bd10a186c6b683cfdaac720b10f1101fc99507
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2021-11-20 14:56:58 +0700

    thiện success update database

---
+++ DB/deleteFile.sql
@@ -0,0 +1,28 @@
+use CLOCKSTOREWEB
+go
+delete from ChiTietPNs
+go
+delete from ChiTietHDs
+go
+delete from HoaDons
+go
+delete from PhieuNhaps
+go
+delete from SanPhams
+go
+delete from LoaiSanPhams
+go
+delete from KieuDays
+go
+delete from KieuMays
+go
+delete from ThuongHieus
+go
+delete from NCCs
+go
+delete from KhachHangs
+go
+delete from NhanViens
+go
+delete from Quyens
+go
