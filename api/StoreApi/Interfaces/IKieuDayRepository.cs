f4cdf10c0e4061a8f7a758687c18230adb8cb1c4
Thien1711 <75517112+Thien1711@users.noreply.github.com>
2021-11-22 06:10:31 +0700

    ác

--- api/StoreApi/Interfaces/IKieuDayRepository.cs
+++ api/StoreApi/Interfaces/IKieuDayRepository.cs
@@ -2,11 +2,11 @@ namespace StoreApi.Interfaces
 {
     public interface KieuDayRepository
     {
-        SanPham KieuDay_Add(KieuDay sp); 
+        KieuDay KieuDay_Add(KieuDay sp); 
         KieuDay KieuDay_GetById(int id); 
         IEnumerable<KieuDay> KieuDay_GetAll();
         KieuDay KieuDay_Update(KieuDay NV); 
         void KieuDay_Delete(KieuDay NV); 
-        IEnumerable<KieuDay> SanPham_FilterAdmin(string search, string sort, int pageIndex, int pageSize, out int count);
+        IEnumerable<KieuDay> KieuDay_FilterAdmin(string search, string sort, int pageIndex, int pageSize, out int count);
     }
 }
